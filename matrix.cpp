e399b9ea7bc21d5ed6b63b4e20d76c68d63f525e
tbenzing <tbenzing4567@gmail.com>
2025-04-16 16:18:19 -0400

    Started programming

---
+++ matrix.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+class Matrix {
+public:
+   Bool add(const vector<vector<int>>& a, const vector<vector<int>>& b) {
+    if ((a.size() != b.size()) || (a[0].size() != b[0].size)) {
+
+    }
+  }
+};
+
+// ---------------------------------------------
+// Matrix Class Implementation
+// ---------------------------------------------
+// Implement constructor(s)
+// Implement get/set methods
+// Implement add, subtract, multiply
+// Implement print
+
+// ---------------------------------------------
+// Main Function for Testing
+// ---------------------------------------------
+int main() {
+  // Create test matrices
+
+  // Print original matrices
+
+  // Perform addition, multiplication, etc.
+
+  // Print results
+
+  return 0;
+}
