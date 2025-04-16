4cd23ec9e904bc1fbe6f043f3d75accf953325ca
tbenzing <tbenzing4567@gmail.com>
2025-04-16 16:45:49 -0400

    Updated code

--- matrix.cpp
+++ matrix.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <stdexcept>
 
 using namespace std;
 
 class Matrix {
 public:
-   Bool add(const vector<vector<int>>& a, const vector<vector<int>>& b) {
-    if ((a.size() != b.size()) || (a[0].size() != b[0].size)) {
-
-    }
-  }
+  bool add(const vector<vector<int>>& a, const vector<vector<int>>& b);
+  void print(const vector<vector<int>>& a);
 };
 
-// ---------------------------------------------
-// Matrix Class Implementation
-// ---------------------------------------------
-// Implement constructor(s)
-// Implement get/set methods
-// Implement add, subtract, multiply
-// Implement print
-
-// ---------------------------------------------
-// Main Function for Testing
-// ---------------------------------------------
 int main() {
+  //create object
+  Matrix calc();
   // Create test matrices
-
+  vector<vector<int>> matrix1 = { {1, 0, 0},{0, 1, 0},{0, 0, 1} };
+  vector<vector<int>> matrix2 = { {0, 1, 1},{1, 0, 1},{1, 1, 0} };
   // Print original matrices
-
+  calc.print(matrix1);
   // Perform addition, multiplication, etc.
 
   // Print results
 
   return 0;
+}
+
+bool Matrix::add(const vector<vector<int>>& a, const vector<vector<int>>& b) {
+  //check that rows and columns are same size
+  if ((a.size() != b.size()) || (a[0].size() != b[0].size())) {
+    cout << "Matrices must be same size to add" << endl;
+    return false;
+  }
+  //a is the vector being added to, so we want to add every value of b
+  //to its corresponding value of a
+  for (unsigned int i = 0; i < a.size(); i++) {
+    for (unsigned int j = 0; j < a[0].size(); j++) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void Matrix::print(const vector<vector<int>>& a) {
+  cout << endl;
+  for (unsigned int i = 0; i < a.size(); i++) {
+    for (unsigned int j = 0; j < a[0].size(); j++) {
+      cout << a[i][j];
+    }
+    cout << endl;
+  }
 }
