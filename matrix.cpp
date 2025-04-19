dbd42b07efe67b0d2400e6d70ebdc5e396da8ed0
tbenzing <tbenzing4567@gmail.com>
2025-04-18 22:27:45 -0400

    Finished main tests and full code functionality

--- matrix.cpp
+++ matrix.cpp
@@ -12,7 +12,8 @@ class Matrix {
   public:
   Matrix(vector<vector<int>>);
   bool add(Matrix b); //adds b to current matrix, returns false if fails
-  bool mult(Matrix b); //multiplies matrix by b (matrix multiplcation)
+  void transpose(); //transposes the matrix
+  bool mult(Matrix b); //multiplies matrix by b (matrix multiplcation), returns false if fails
   bool mult(int b); //multiplies matrix by b (scalar multiplication)
   void print(); //prints matrix
 };
@@ -24,6 +25,7 @@ int main() {
   Matrix matrix2 = Matrix({ {0, 1, 1},{1, 0, 1},{1, 1, 0} });
   Matrix matrix3 = Matrix({ {1, 0, 0},{0, 1, 0} });
   Matrix matrix4 = Matrix({ {0, 1},{1, 0},{1, 1} });
+  Matrix matrix5 = Matrix({ {1, 2, 3},{1, 2, 3},{1, 2, 3} });
   // Print original matrices
   matrix1.print();
   matrix2.print();
@@ -39,6 +41,36 @@ int main() {
 
   matrix1.mult(2);
   matrix1.print();
+
+  matrix5.print();
+  matrix5.transpose();
+  matrix5.print();
+
+  cout << "NOOOOW FOR THE GRAAAAAND FINALE!!!" << endl;
+  Matrix matrixA = Matrix({ {6, 4},{8, 3} });
+  Matrix matrixB = Matrix({ {1, 2, 3},{4, 5, 6} });
+  Matrix matrixC = Matrix({ {2, 4, 6},{1, 3, 5} });
+  cout << "Matrix A: ";
+  matrixA.print();
+  cout << endl;
+  cout << "Matrix B: ";
+  matrixB.print();
+  cout << endl;
+  cout << "Matrix C: ";
+  matrixC.print();
+  cout << endl;
+  cout << "Multriply B by 3" << endl;
+  matrixB.mult(3);
+  matrixB.print();
+  cout << "Transpose C" << endl;
+  matrixC.transpose();
+  matrixC.print();
+  cout << "Multriply B by C" << endl;
+  matrixB.mult(matrixC);
+  matrixB.print();
+  cout << "Add B to A" << endl;
+  matrixA.add(matrixB);
+  matrixA.print();
   return 0;
 }
 
@@ -64,6 +96,25 @@ bool Matrix::add(Matrix b) {
   return true;
 }
 
+void Matrix::transpose() {
+  //create a temp matrix
+  vector<vector<int>> temp(m_columns, vector<int>(m_rows, 0));
+  //iterate through matrix
+  for (unsigned int i = 0; i < m_rows; i++) {
+    for (unsigned int j = 0; j < m_columns; j++) {
+      //if were on the diagonal, put it in the same place
+      if(i == j) {
+        temp[i][j] = m_matrix[i][j];
+      } else {
+        temp[j][i] = m_matrix[i][j];
+      }
+    }
+  }
+  m_matrix = temp;
+  m_rows = temp.size();
+  m_columns = temp[0].size();
+}
+
 bool Matrix::mult(Matrix b) {
   //check that the num of columns in a are equal to the num of rows in b
   if (m_columns != b.m_rows) {
@@ -80,7 +131,7 @@ bool Matrix::mult(Matrix b) {
     for(unsigned int j = 0; j < b.m_columns; j++) {
       //for each value in each --> k is our value
       for(unsigned int k = 0; k < m_columns; k++) {
-        temp[i][j] += (m_matrix[i][k]*b.m_matrix[j][k]);
+        temp[i][j] += (m_matrix[i][k]*b.m_matrix[k][j]);
       }
     }
   }
@@ -104,8 +155,9 @@ void Matrix::print() {
   cout << endl;
   for (unsigned int i = 0; i < m_rows; i++) {
     for (unsigned int j = 0; j < m_columns; j++) {
-      cout << m_matrix[i][j];
+      cout << m_matrix[i][j] << " ";
     }
     cout << endl;
   }
+  cout << endl;
 }
